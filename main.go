14e5b1411e042bd313e67df2a261b688b1f860a2
rlawlsgus <jh0623.kim@gmail.com>
2020-05-02 23:14:20 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age int
+	favFood []string
+}
+
 func main() {
-	kim := map[string]string{"name": "kim", "age": "12"}
-	for _, value := range kim {
-		fmt.Println(value)
-	}
+	favFood := []string{"gukbap", "dupbap"}
+	kim := person{name: "kim", age: 17, favFood: favFood}
+	fmt.Println(kim)
 }
