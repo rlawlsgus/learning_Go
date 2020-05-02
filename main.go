55986cb7dd2193bfe8b1896db3214a98b31c6574
rlawlsgus <jh0623.kim@gmail.com>
2020-05-02 23:08:47 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"kim", "nico", "lynn"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	kim := map[string]string{"name": "kim", "age": "12"}
+	for _, value := range kim {
+		fmt.Println(value)
+	}
 }
