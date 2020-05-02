1ff4c84a410ea06eb533595834e0aa3060cb4ac6
rlawlsgus <jh0623.kim@gmail.com>
2020-05-02 22:42:37 +0900

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch koreanAge := age + 2; {
+	case koreanAge < 18:
+		return false
+	case koreanAge == 18:
+		return true
+	case koreanAge > 50:
 		return false
 	}
 	return true
 }
 
 func main() {
-	fmt.Println(canIDrink(16))
+	fmt.Println(canIDrink(15))
 }
